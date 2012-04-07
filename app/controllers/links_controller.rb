7b26bbb43d577348d6e9d43523df563dc0f4cbc4
Vitaly Tatarintsev <Kalastiuz@gmail.com>
2012-04-07 17:07:48 +0300

    sort by days left then by seller name

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -11,7 +11,7 @@ class LinksController < ApplicationController
     @links = @links.where(:id => Link.by_seller(params[:seller])) if params[:seller].present?
     @links = @links.where(:placement_id => params[:placement]) if params[:placement].present?
     @links = @links.where(:id => Link.by_payment_method(params[:pm])) if params[:pm].present?
-    @links = @links.sort_by { |link| link.days_left }
+    @links = @links.sort_by(&:days_left).sort_by(&:seller_name)
 
     set_meta_tags :title => I18n.t(:links_title)
   end
