dc095e56d6169597a678ce90126e1dd88c3fd082
Vitaly Tatarintsev <Kalastiuz@gmail.com>
2012-07-25 08:56:05 +0300

    fix our sites select

--- app/models/our_site.rb
+++ app/models/our_site.rb
@@ -8,5 +8,18 @@ class OurSite < ActiveRecord::Base
 
   has_and_belongs_to_many :access_list_users, :join_table => :access_list_users_our_sites, :class_name => "User"
 
+  delegate :name, to: :category, prefix: true
+
   scope :visible_for, lambda { |user_id| joins(:access_list_users).where('users.id' => user_id) }
+
+  def self.collection_for(user)
+    our_sites = self.where(category_id: Category.visible_for(user))
+    collection = {}
+    our_sites.each do |os|
+      title = [os.category_name, os.name].join(" #{I18n.t(:arrow)} ")
+      collection[title] = os.id
+    end
+
+    collection
+  end
 end
